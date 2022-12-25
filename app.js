a73038e2f0483094be947ad29031e5a75328c405
Phazerous <nphazerous@gmail.com>
2022-12-26 02:42:51 +0600

    Authentication | Level 1

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/login', (req, res) => res.render('login'));
+
+app.get('/register', (req, res) => res.render('register'));
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('secrets');
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    if (!foundUser) return;
+
+    if (foundUser.password !== password) return;
+
+    res.render('secrets');
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Server runs on port 3000');
+});
