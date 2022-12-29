d91f7f4c1b84c42f4d9a2b09182fcc66460d7b48
Phazerous <nphazerous@gmail.com>
2022-12-29 23:52:51 +0600

    Authentication | Level 3 Md5 Hashing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { User } = require('./db');
+const md5 = require('md5');
 
 const app = express();
 
@@ -21,7 +22,7 @@ app.get('/register', (req, res) => res.render('register'));
 app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save((err) => {
@@ -35,7 +36,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
 mongoose.set('strictQuery', false);
 
 mongoose.connect('mongodb://127.0.0.1:27017/userDB');
@@ -7,12 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-});
-
-const encryptionKey = 'ClippersForSale';
-userSchema.plugin(encrypt, {
-  secret: encryptionKey,
-  encryptedFields: ['password'],
 });
 
 const User = mongoose.model('User', userSchema);
